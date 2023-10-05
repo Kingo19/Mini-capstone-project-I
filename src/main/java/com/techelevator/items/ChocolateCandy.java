723c1b942c8a0fa5766676a4d670feab70b6828c
Ian Moeckel <iangmoeckel@gmail.com>
2023-10-05 17:40:08 -0400

    re-push to fix

--- src/main/java/com/techelevator/items/ChocolateCandy.java
+++ src/main/java/com/techelevator/items/ChocolateCandy.java
@@ -4,8 +4,5 @@ public class ChocolateCandy extends CandyStoreItem {
     public ChocolateCandy(String name, String ID, double price, String isWrapped) {
         super(name, ID, price, isWrapped);
         setDescription("Chocolate Confectionary");
-
-
-
     }
 }
