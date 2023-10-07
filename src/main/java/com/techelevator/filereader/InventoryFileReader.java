25d4ffaa0a3c0c85dce23b8061db22f9047d466f
Nigus Gebremariam <kingsgm19@gmail.com>
2023-10-07 16:49:18 -0400

    Shopping cart and half inventory test done

--- src/main/java/com/techelevator/filereader/InventoryFileReader.java
+++ src/main/java/com/techelevator/filereader/InventoryFileReader.java
@@ -59,4 +59,5 @@ public class InventoryFileReader {
         }
         return candyItems;
     }
+
 }
--- src/test/java/com/techelevator/InventoryTest.java
+++ src/test/java/com/techelevator/InventoryTest.java
@@ -1,13 +1,26 @@
 package com.techelevator;
 
+import com.techelevator.items.CandyStoreItem;
 import org.junit.*;
 
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 public class InventoryTest {
-
-    @Test
-    public void inventory_ingests_txt_file() {
-
+    private Inventory inventory;
+    @Before
+    public void setUp() {
+        // Create an instance of Inventory before each test
+        inventory = new Inventory();
     }
 
+    @Test
+    public void retrieveListOfInventory_returnsNonEmptyList() {
+        List<CandyStoreItem> inventoryList = inventory.retrieveListOfInventory();
+        assertNotNull(inventoryList);
+        assertTrue(inventoryList.size() > 0);
+    }
 
 }
